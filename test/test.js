dfce01c91b6806c90e0972441be6e1055cc075de
Ines Guerrero <inesgs@gmail.com>
2019-11-28 13:51:28 +0000

    use before each and after each

--- test/test.js
+++ test/test.js
@@ -7,8 +7,27 @@ const assert = require("assert");
 // PEDING TEST - it is a test that has nothing.
 // Sometimes it helps to create a test for a function that you may not be ready to test or that may not exist yet.
 
+// BEFORE AND AFTER functions - help you set up and turn down tests.
+// BEFORE EACH AND AFTER EACH happen before each test inside a block
+// Allow you to create subs and variables that are needed for the tests.
+
 describe("file to be tested", () => {
   context("function to execute", () => {
+    before(() => {
+      console.log("=======before");
+    });
+
+    after(() => {
+      console.log("=======after");
+    });
+
+    beforeEach(() => {
+      console.log("=======beforeEach");
+    });
+
+    afterEach(() => {
+      console.log("=======afterEach");
+    });
     it("should do something", () => {
       assert.equal(1, 1);
     });
